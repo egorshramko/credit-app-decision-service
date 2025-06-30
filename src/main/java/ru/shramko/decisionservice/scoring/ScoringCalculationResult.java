a0872bb857be5a9d80c2b7538621c1c4e1d1fbcf
Egor Shramko <egorii2001@gmail.com>
2025-07-01 00:48:25 +0700

    Added NoArgsConstructor to ScoringCalculationResult

--- src/main/java/ru/shramko/decisionservice/scoring/ScoringCalculationResult.java
+++ src/main/java/ru/shramko/decisionservice/scoring/ScoringCalculationResult.java
@@ -1,10 +1,12 @@
 package ru.shramko.decisionservice.scoring;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScoringCalculationResult {
 	private String name;
 	private String value;
