3ff70191d3be3be1a55d7e945b4d6dcff5b54a97
Egor Shramko <egorii2001@gmail.com>
2025-07-01 00:52:43 +0700

    Created SexScoringCalculation class

---
+++ src/main/java/ru/shramko/decisionservice/scoring/impl/SexScoringCalculation.java
@@ -0,0 +1,36 @@
+package ru.shramko.decisionservice.scoring.impl;
+
+import ru.shramko.decisionservice.common.enums.Sex;
+import ru.shramko.decisionservice.scoring.ScoringCalculation;
+import ru.shramko.decisionservice.scoring.ScoringCalculationResult;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class SexScoringCalculation implements ScoringCalculation {
+
+	private final Sex sex;
+	
+	@Override
+	public ScoringCalculationResult calculate() {
+		
+		ScoringCalculationResult result = new ScoringCalculationResult();
+		result.setName("sex");
+		
+		switch (sex) {
+		case Sex.MALE:
+			result.setValue("MALE");
+			result.setScore(Double.valueOf(3.0));
+			break;
+		case Sex.FEMALE:
+			result.setValue("FEMALE");
+			result.setScore(Double.valueOf(7.0));
+			break;
+		}
+		
+		return result;
+	}
+
+}
