810aab44f9ddf01677855ae3ac9edb6dbbf30436
Egor Shramko <egorii2001@gmail.com>
2025-07-01 02:14:02 +0700

    Created ScoringResult class

---
+++ src/main/java/ru/shramko/decisionservice/model/ScoringResult.java
@@ -0,0 +1,32 @@
+package ru.shramko.decisionservice.model;
+
+import java.util.List;
+
+import lombok.NoArgsConstructor;
+import ru.shramko.decisionservice.scoring.ScoringCalculationResult;
+
+@NoArgsConstructor
+public class ScoringResult {
+	private double value = 0;
+	private List<ScoringCalculationResult> scoringCalculationResults;
+	
+	public double getValue() {
+		return value;
+	}
+	
+	public boolean addScoringCalculationResult(ScoringCalculationResult scoringResult) {
+		value += scoringResult.getScore().doubleValue();
+		return scoringCalculationResults.add(scoringResult);
+	}
+	
+	public boolean removeScoringCalculationResult(ScoringCalculationResult scoringResult) {
+		
+		if (scoringCalculationResults.remove(scoringResult)) {
+			value -= scoringResult.getScore().doubleValue();
+			return true;
+		}
+		
+		return false;
+
+	}
+}
