e74a720bdfc296d2141165231e3ba6a4df06e661
Egor Shramko <egorii2001@gmail.com>
2025-07-01 00:12:18 +0700

    Initial commit

---
+++ src/main/java/ru/shramko/decisionservice/DecisionserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.shramko.decisionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionserviceApplication.class, args);
+	}
+
+}
