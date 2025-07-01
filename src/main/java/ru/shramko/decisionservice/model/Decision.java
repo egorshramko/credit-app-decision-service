56711ca47de89bd447227be8595ae084e67be6a5
Egor Shramko <egorii2001@gmail.com>
2025-07-01 21:13:11 +0700

    Added general decision field in Desicion class

--- src/main/java/ru/shramko/decisionservice/model/Decision.java
+++ src/main/java/ru/shramko/decisionservice/model/Decision.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 import lombok.Data;
 import ru.shramko.decisionservice.check.CheckResult;
+import ru.shramko.decisionservice.check.enums.CheckResultType;
 import ru.shramko.decisionservice.scoring.ScoringCalculationResult;
 
 @Data
 public class Decision {
+	private CheckResultType decision = CheckResultType.APPROVED;
 	private List<CheckResult> checkResults = new ArrayList<>();
 	private ScoringResult scoringResult = new ScoringResult();
 	
 	public boolean addCheckResult(CheckResult checkResult) {
+		if (checkResult.getResult() == CheckResultType.REJECTED) {
+			decision = CheckResultType.REJECTED;
+		}
 		return checkResults.add(checkResult);
 	}
 	
